ded31ed5466a139fc993acdfecf650b2cb066e03
ErnAbr <ern_abr@yahoo.com>
2024-07-10 13:47:37 +0300

    authRouter

--- server/config/configRoutes.js
+++ server/config/configRoutes.js
@@ -1,11 +1,13 @@
 const categoriesRouter = require("../features/categories/router");
 const bookingRouter = require("../features/bookings/router");
 const businessRouter = require("../features/businesses/router");
+const authRouter = require("../features/auth/router");
 
 const configRoutes = (server) => {
   server.use(categoriesRouter);
   server.use(bookingRouter);
   server.use(businessRouter);
+  server.use(authRouter);
 };
 
 module.exports = configRoutes;
