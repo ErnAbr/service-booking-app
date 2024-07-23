e5b0a85b40e97cd407e3ca3f4e194797a1babe9b
ErnAbr <ern_abr@yahoo.com>
2024-07-23 22:07:10 +0300

    heroku setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,11 +3,20 @@ import { connectDB } from "./libs/database";
 import { configMiddlewares } from "./config/configMiddlewares";
 import { configRoutes } from "./config/configRoutes";
 import { envVariables } from "./config/configEnvVariables";
+import path from "path";
 
 const server = express();
 connectDB();
+
+server.use(express.json({ limit: "50mb" }));
+server.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
 configMiddlewares(server);
 configRoutes(server);
+
+server.get("*", (_, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist", "index.html"));
+});
 
 server.listen(envVariables.server.port, () => {
   // eslint-disable-next-line no-console
