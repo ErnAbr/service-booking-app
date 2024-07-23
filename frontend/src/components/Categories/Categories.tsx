341de3b748f998d22e454a8a9b17f665cf2a875b
ErnAbr <ern_abr@yahoo.com>
2024-07-23 23:38:11 +0300

    bug search

--- frontend/src/components/Categories/Categories.tsx
+++ frontend/src/components/Categories/Categories.tsx
@@ -12,13 +12,8 @@ interface CategoryProps {
 
 export const Categories = ({ iconSize }: CategoryProps) => {
   const [selectedCategory, setSelectedCategory] = useState<string>("");
-  const { data: categories, error, isLoading } = useCategories();
+  const { data: categories } = useCategories();
   const navigate = useNavigate();
-
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error loading categories</div>;
-
-  console.log('Categories Data:', categories); // Log the categories data
 
   const handleCategoryClick = (categoryName: string) => {
     const toggleCategory =
@@ -31,33 +26,25 @@ export const Categories = ({ iconSize }: CategoryProps) => {
     <>
       <h3 className={styles.categoryHeading}>Categories</h3>
       <div className={styles.categoryContainer}>
-        {Array.isArray(categories) && categories.length > 0 ? (
-          categories.map((category, index) => {
-            console.log(`Category ${index}:`, category); // Log each category
-            const IconComponent = iconMapping[category.imageUrl];
-            if (!IconComponent) {
-              console.error(`No icon found for ${category.imageUrl}`);
-              return null; // Skip rendering if no icon is found
-            }
-            return (
-              <Button
-                key={index}
-                variant="square"
-                onClick={() => handleCategoryClick(category.categoryName)}
-                isSelected={selectedCategory === category.categoryName.toLowerCase()}
-              >
-                <div className={styles.buttonContent}>
-                  <IconContext.Provider value={{ color: category.backgroundColor, size: iconSize }}>
-                    <IconComponent />
-                  </IconContext.Provider>
-                  {category.categoryName}
-                </div>
-              </Button>
-            );
-          })
-        ) : (
-          <p>No categories available</p>
-        )}
+        {categories?.map((category, index) => {
+          // const IconComponent = iconMapping[category.imageUrl];
+          return (
+            <Button
+              key={index}
+              variant="square"
+              onClick={() => handleCategoryClick(category.categoryName)}
+              isSelected={selectedCategory === category.categoryName.toLowerCase()}
+            >
+              <div className={styles.buttonContent}>
+                <IconContext.Provider value={{ color: category.backgroundColor, size: iconSize }}>
+                  {/* <IconComponent /> */}
+                  TEST
+                </IconContext.Provider>
+                {category.categoryName}
+              </div>
+            </Button>
+          );
+        })}
       </div>
     </>
   );
