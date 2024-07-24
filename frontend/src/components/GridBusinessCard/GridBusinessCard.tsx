ddc729081df9a73ab4c21227b696700643250b0e
ErnAbr <ern_abr@yahoo.com>
2024-07-24 08:36:10 +0300

    update

--- frontend/src/components/GridBusinessCard/GridBusinessCard.tsx
+++ frontend/src/components/GridBusinessCard/GridBusinessCard.tsx
@@ -24,7 +24,7 @@ export const GridBusinessCard = ({ filteredItems, page, itemsPerPage }: CardCate
 
   const startIndex = (page - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
-  const paginatedItems = filteredItems?.slice(startIndex, endIndex);
+  const paginatedItems = filteredItems?.slice(startIndex, endIndex) || [];
 
   return (
     <Grid
@@ -33,14 +33,18 @@ export const GridBusinessCard = ({ filteredItems, page, itemsPerPage }: CardCate
       headingClass={styles.heading}
       gridClass={styles.grid}
     >
-      {paginatedItems?.map((props) => (
-        <CardBusiness
-          key={props.id}
-          isFavorite={favorites.includes(props.id)}
-          toggleFavorite={() => toggleFavorite(props.id)}
-          {...props}
-        />
-      ))}
+      {paginatedItems.length > 0 ? (
+        paginatedItems.map((props) => (
+          <CardBusiness
+            key={props.id}
+            isFavorite={favorites.includes(props.id)}
+            toggleFavorite={() => toggleFavorite(props.id)}
+            {...props}
+          />
+        ))
+      ) : (
+        <div>No businesses to display</div>
+      )}
     </Grid>
   );
 };
