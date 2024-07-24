3d14b5d88e55344d66b8287ba44b20d5d8fe9e7e
ErnAbr <ern_abr@yahoo.com>
2024-07-24 13:57:49 +0300

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Routes } from "./navigation/routes/router";
 import { AppInitializer } from "./utils/AppInitializer";
-import "@/styles/BaseStyles.scss";
+import "./styles/BaseStyles.scss";
 
 const queryClient = new QueryClient();
 
